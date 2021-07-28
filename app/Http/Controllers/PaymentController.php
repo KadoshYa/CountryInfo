d59bca454acacf0411d7362468ab503786ae8cb9
Kidus Yared <kidyared363@gmail.com>
2021-07-28 14:29:43 +0300

    ip change

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,7 +12,7 @@ class PaymentController extends Controller
         // $data['name'] = $request->name;  
         // $data['email'] = $request->email;  
         // $data['amount'] = $request->amount;
-        $data['link'] = "http://192.168.0.123:4000/payment?name=".$request->name."&email=".$request->email."&amount=".$request->amount;  
+        $data['link'] = "https://chapacheckout.herokuapp.com/payment?name=".$request->name."&email=".$request->email."&amount=".$request->amount;  
 
         return $data;
     }
