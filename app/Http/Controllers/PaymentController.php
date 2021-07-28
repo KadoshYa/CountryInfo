8bcefc6787c9644694147199ed2ee2b5fc54a60d
Kidus Yared <kidyared363@gmail.com>
2021-07-28 10:23:43 +0300

    ip change

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,7 +12,7 @@ class PaymentController extends Controller
         // $data['name'] = $request->name;  
         // $data['email'] = $request->email;  
         // $data['amount'] = $request->amount;
-        $data['link'] = "http://127.0.0.1:4000/payment?name=".$request->name."&email=".$request->email."&amount=".$request->amount;  
+        $data['link'] = "http://192.168.0.123:4000/payment?name=".$request->name."&email=".$request->email."&amount=".$request->amount;  
 
         return $data;
     }
