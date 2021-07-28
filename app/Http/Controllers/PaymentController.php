525fc13ae4bb4e007f42b2a75407064f32528446
Kidus Yared <kidyared363@gmail.com>
2021-07-28 15:53:28 +0300

    last change

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,7 +12,7 @@ class PaymentController extends Controller
         // $data['name'] = $request->name;  
         // $data['email'] = $request->email;  
         // $data['amount'] = $request->amount;
-        $data['link'] = "https://chapacheckout.herokuapp.com/payment?name=".$request->name."&email=".$request->email."&amount=".$request->amount;  
+        $data['link'] = "https://chapacheckout.herokuapp.compayment?name=".$request->name."&email=".$request->email."&amount=".$request->amount;  
 
         return $data;
     }
