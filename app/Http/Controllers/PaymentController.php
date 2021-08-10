7e59c06dacbfb613cd009ec75ad7613ccfc0f51a
Kidus Yared <kidyared363@gmail.com>
2021-08-10 14:50:37 +0300

    another one

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -16,4 +16,19 @@ class PaymentController extends Controller
 
         return $data;
     }
+    public function payment (Request $request)
+    {
+        $response = Http::asForm()->post('https://paymentuat.psseth.com/ss-ecom-merchant-kit/buyForm', [
+            'PSP ID' => 'PSP_001',
+            'MPI ID' => 'mpi-test',
+            'Merchant ID' => '000030010400090',
+            'MCC' => '6011',
+            'Merchant Kit ID' => 'mki-test',
+            'Authentification Token' => 'ABA50477AF5266E2E053CF01010A0914',
+            'Currency' => '230 ',
+        ]);
+
+        dd($response);
+        return $payment;
+    }
 }
